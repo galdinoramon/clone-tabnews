201c76e119f875611d0c6faded32ec927e7b24d2
galdinoramon <73455988+galdinoramon@users.noreply.github.com>
2024-11-21 01:11:12 +0000

    feat: first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './pages.module.css';
+
+function Home() {
+    return (
+        <>
+        <h1 className={styles.title}>Bem-vindo!</h1>
+        <h2>Agora já sei como colocar uma página no ar!</h2>
+        </>
+)
+}
+
+
+export default Home;
