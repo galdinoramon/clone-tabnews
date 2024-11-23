0b228c3f25ef3a14e7aa6d6d51fb6d477490db74
galdinoramon <73455988+galdinoramon@users.noreply.github.com>
2024-11-23 20:12:32 +0000

    design changes

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import styles from './pages.module.css';
 
 function Home() {
     return (
-        <>
+        <div className={styles.header}>
         <h1 className={styles.title}>Bem-vindo!</h1>
         <h2>Agora já sei como colocar uma página no ar!</h2>
-        </>
+        </div>
 )
 }
 
